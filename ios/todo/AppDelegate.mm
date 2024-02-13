a3abaa3891837851ea644e68acedde1960774f91
tedd911 <tedd911@datau.co.kr>
2024-02-13 12:00:55 +0900

    feat(firebase): 파이어베이스 코어 라이브러리 설치

--- ios/todo/AppDelegate.mm
+++ ios/todo/AppDelegate.mm
@@ -2,15 +2,17 @@
 
 #import <React/RCTBundleURLProvider.h>
 
+#import <Firebase.h>
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [FIRApp configure];
   self.moduleName = @"todo";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
-
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
