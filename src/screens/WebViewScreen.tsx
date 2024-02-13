fb087b66950b534702f07900db16b166b77cdc44
tedd911 <tedd911@datau.co.kr>
2024-02-13 18:16:11 +0900

    feat(react-native-webview): webview -> rn success

--- src/screens/WebViewScreen.tsx
+++ src/screens/WebViewScreen.tsx
@@ -1,6 +1,16 @@
 import WebView from 'react-native-webview';
+import {Alert} from 'react-native';
 
 const WebViewScreen = () => {
-  return <WebView source={{uri: 'https://dev-www.autocall.ai'}} />;
+  const handleOnMessage = (nativeEvent: any) => {
+    console.log('nativeEvent', nativeEvent);
+    Alert.alert('nativeEvent', JSON.stringify(nativeEvent));
+  };
+  return (
+    <WebView
+      onMessage={handleOnMessage}
+      source={{uri: 'http://5.0.50.118:3000'}}
+    />
+  );
 };
 export default WebViewScreen;
