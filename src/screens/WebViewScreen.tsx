f1534c7c377f75cc1aa0a09dd9568c29596c90ed
tedd911 <tedd911@datau.co.kr>
2024-02-14 17:19:43 +0900

    feat(react-native-cookie): add react-native-cookie lib

--- src/screens/WebViewScreen.tsx
+++ src/screens/WebViewScreen.tsx
@@ -1,31 +1,41 @@
 import WebView from 'react-native-webview';
 import {Alert} from 'react-native';
+import CookieManager from '@react-native-cookies/cookies';
 
 interface RNToWebviewMessage {
   type: 'RN' | 'WEBVIEW';
   data: any;
 }
+CookieManager.set('http://5.0.50.135:3000', {
+  name: 'customCookie',
+  value: 'tedd911',
+});
+// CookieManager.clearAll();
 const WebViewScreen = () => {
   const rnToWebview: RNToWebviewMessage = {
     type: 'RN',
     data: {message: 'Hello React!'},
   };
+  const beforeContentLoaded = `
+    window.isNativeApp = true;
+    true;
+`;
   const handleOnMessage = (event: any) => {
     console.log('nativeEvent', event);
     Alert.alert('nativeEvent', JSON.stringify(event.nativeEvent.data));
   };
-  const runFirst = `window.isNativeApp = true;
-true;
-`;
   return (
     <WebView
-      onMessage={handleOnMessage}
+      injectedJavaScriptBeforeContentLoaded={beforeContentLoaded}
       source={{
-        uri: 'http://5.0.50.118:3000',
-        headers: {'setup-rn-header': 'this-is-custom-header'},
-        Cookie: 'setup-rn-cookie=this-is-custom-cookie',
+        uri: 'http://5.0.50.135:3000',
+        headers: {
+          'setup-rn-header': 'this-is-custom-header',
+          Cookie: 'cookie1=this-is-custom-cookie;',
+        },
       }}
       sharedCookiesEnabled={true}
+      onMessage={handleOnMessage}
     />
   );
 };
